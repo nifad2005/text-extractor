3031e3a03e5692de6cb468c93463150fc8a65002
Md Nifad Uzzaman <nifaduzzaman2005@gmail.com>
2026-02-28 14:51:54 +0800

    file name issue solved

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ from PyPDF2 import PdfReader
 
 #! PDF Extractor 
 def PDF_data_extractor():
-    reader = PdfReader("book.pdf")
+    reader = PdfReader("sample_pdf.pdf")
     for page in reader.pages:
-        with open("pdf_output.txt","a", encoding="utf-8") as f:
+        with open("output_pdf.txt","a", encoding="utf-8") as f:
             f.write(page.extract_text())
-    print("PDF has been written in 'pdf_output.txt'.✅")
+    print("PDF has been written in 'output_pdf.txt'.✅")
 
 #! Text Extractor
 def text_data_extractor():
-    with open("sample_data.txt", "r", encoding="utf-8") as f:
+    with open("sample_text.txt", "r", encoding="utf-8") as f:
         data = f.read()
-        with open("txt_output.txt","a", encoding="utf-8") as f:
+        with open("output_text.txt","a", encoding="utf-8") as f:
             f.write(data)
-    print("Text has been written in 'txt_output.txt'.✅")
+    print("Text has been written in 'output_text.txt'.✅")
 
 if __name__ == "__main__":
     PDF_data_extractor()
